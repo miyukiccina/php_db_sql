b8b34f3046d67ba4193329c2e33e035b31a0e8d8
miyukiccina <80190927+miyukiccina@users.noreply.github.com>
2021-03-23 23:17:46 +0900

    first commit

---
+++ data.php
@@ -0,0 +1,51 @@
+<?php
+if(
+    !isset($_POST["name"]) || $_POST["name"]=="" ||
+    !isset($_POST["area"]) || $_POST["area"]=="" ||
+    !isset($_POST["area2"]) || $_POST["area2"]=="" ||
+    !isset($_POST["category"]) || $_POST["category"]=="" ||
+    !isset($_POST["adress"]) || $_POST["adress"]=="" ||
+    !isset($_POST["comment"]) || $_POST["comment"]==""
+){
+    exit('ParamError');
+}
+
+$name = $_POST["name"];
+$area = $_POST["area"];
+$area2 = $_POST["area2"];
+$category = $_POST["category"];
+$adress = $_POST["adress"];
+$comment = $_POST["comment"];
+
+// データベースに接続する
+try {
+$pdo=new PDO('mysql:dbname=lamiapatria;charset=utf8;host=localhost','root','root');
+} catch (PDOException $e) {
+    exit('DbConnectError:'.$e->getMessage());
+}
+
+
+// データ登録SQL
+$sql = "INSERT INTO italian_restaurant(number, name, area, area2, category, adress, comment)VALUES(NULL, :a1, :a2, :a3, :a4, :a5, :a6)";
+
+$stmt = $pdo->prepare($sql);
+
+$stmt->bindValue(':a1', $name, PDO::PARAM_STR);
+$stmt->bindValue(':a2', $area, PDO::PARAM_STR);
+$stmt->bindValue(':a3', $area2, PDO::PARAM_STR);
+$stmt->bindValue(':a4', $category, PDO::PARAM_STR);
+$stmt->bindValue(':a5', $adress, PDO::PARAM_STR);
+$stmt->bindValue(':a6', $comment, PDO::PARAM_STR);
+$status = $stmt->execute();
+
+// // データ登録処理後
+if($status==false){
+
+    $error = $stmt->errorInfo();
+    exit("QueryError:".$error[2]);
+}else{
+    header("Location: index.php");
+    exit;
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+    <h1 id="title">La Nostra Patria</h1>
+    <div>-La cucina che ti mancava-</div>
+    </header>
+</body>
+</html>
+
+
+<form action="data.php" method="post">
+
+  <p>Name of Restaurant：<input type="text" name="name" size="20"></p>
+  <p>Area：<input type="text" name="area" size="20"></p>
+  <p>Area2：<input type="text" name="area2" size="20"></p>
+  <p>Category：<input type="text" name="category" size="20"></p>
+  <p>Adress：<input type="text" name="adress" size="20"></p>
+  <p>Comment:<textArea name="comment" rows="4" cols="40"></textArea></p>
+
+  
+  <p><input type="submit" value="Register" ></p>
+
+
+</form>
+
+<!-- エリアとカテゴリを選べるようにしたい -->
+  <!-- 写真を登録できるようにしたい -->
